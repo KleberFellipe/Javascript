9812de6f2cc5e6975735962d5066e9ed815542f7
Kleber <kleber.steinke@escola.pr.gov.br>
2025-05-08 11:18:17 -0300

    Treinando prompt alert e console

---
+++ app.js
@@ -0,0 +1,6 @@
+let numero1 = parseInt(prompt("Escreva o primeiro número"));
+let numero2 = parseInt(prompt("Escreva o segundo número"));
+
+let soma = numero1 + numero2;
+
+alert("A soma dos dois números é "+soma);
